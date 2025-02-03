cfcba29a6fdb74a53b05d9f9f2256fa04e1db2c7
suppg022312 <suppg022312@users.noreply.github.com>
2025-02-03 11:05:01 +0000

    Auto-update Mon  3 Feb 11:05:01 UTC 2025

--- model_test_app.py
+++ model_test_app.py
@@ -121,8 +121,8 @@ with gr.Blocks(css=css, theme=gr.themes.Default()) as interface:
 
 if __name__ == "__main__":
     interface.launch(
+        server_port=7862,
         server_name="0.0.0.0",
-        server_port=7861,
         inbrowser=True,
         show_error=True,
         debug=True,
